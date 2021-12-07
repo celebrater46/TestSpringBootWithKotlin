ecbd242bd7a1384dbbb8878dcd719310f14531b3
hideru <jota.16.stigma.world@gmail.com>
2021-12-07 22:34:48 +0900

    first commit

---
+++ src/main/kotlin/com/shirogane/test_211207/Test211207Application.kt
@@ -0,0 +1,11 @@
+package com.shirogane.test_211207
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Test211207Application
+
+fun main(args: Array<String>) {
+	runApplication<Test211207Application>(*args)
+}
